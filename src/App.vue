6184e77813992c731e38e12d9e57755f8fb22d56
Summer <summer.d.howard@gmail.com>
2019-10-06 11:07:12 -0500

    working

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,62 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-autocomplete label="Simple List" :items="components"></v-autocomplete>
+      <v-autocomplete label="Array Of Objects" :items="array" :item-text="name" :item-value="name">
+        <template v-slot:selection="data">
+          <v-chip
+            v-bind="data.attrs"
+            :input-value="data.selected"
+            close
+            @click="data.select"
+            @click:close="remove(data.item)"
+          >{{ data.item.name }}</v-chip>
+        </template>
+
+        <template v-slot:item="{ item }">
+          <v-list-item-avatar
+            color="indigo"
+            class="headline font-weight-light white--text"
+          >{{ item.name.charAt(0) }}</v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.name"></v-list-item-title>
+            <v-list-item-subtitle v-text="item.symbol"></v-list-item-subtitle>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-icon>mdi-coin</v-icon>
+          </v-list-item-action>
+        </template>
+      </v-autocomplete>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      array: [
+        {
+          name: "test name",
+          type: "dragon",
+          likes: "carrots",
+          symbol: "DARGON"
+        }
+      ],
+
+      components: [
+        "Autocompletes",
+        "Comboboxes",
+        "Forms",
+        "Inputs",
+        "Overflow Buttons",
+        "Selects",
+        "Selection Controls",
+        "Sliders",
+        "Textareas",
+        "Text Fields"
+      ]
+    };
+  }
 };
 </script>
